7ae4005a4fd311543967fd527e495be9fba49218
Deminarcis.rb <jake.maher.10@gmail.com>
2022-11-21 22:36:45 +1100

    Added a real README and the installer script

---
+++ Commandant.ps1
@@ -0,0 +1,32 @@
+# if you started this script without admin rights, this will first start a new console with admin rights then run the script below
+# the original console will be closed
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # add -noexit before -encodedcommand to keep the admin console open after the script is run
+    start-process powershell "-encodedcommand $([Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($script:MyInvocation.MyCommand.ScriptBlock)))" -Verb RunAs
+    # comment this out to leave the non admin console open, but you'll not be needing it since a new one will open with admin rights
+    # NOTE!: if you do comment this out, the rest of the script will be executed first as non admin, then again when the admin console appears
+    # you could add another if statement down below saying if you're not admin, do something else, or add an else to this if statment
+    exit
+}
+
+#####
+# BEGIN YOUR SCIPT HERE:
+#####
+
+wget -O wsl_kernel.msi https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
+.\wsl_kernel.msi
+wsl --install
+Write-Output 'The next step may fail if you dont have a Microsoft account logged in to the store or this PC'
+winget install  9PKR34TNCV07 --source msstore
+Write-Output 'Provsioning Kali'
+kali
+wsl -s kali-linux
+wsl --unregister Ubuntu
+wsl sudo apt update
+wsl sudo apt full-upgrade
+wsl sudo apt install kali-tools-top10 wireshark-
+winget install Microsoft.NuGet
+winget install microsoft.powershell
+Install-Package chocolatey
+choco upgrade chocolatey -y
+choco install firefox sysinternals ChocolateyGUI adobereader vlc python 7zip testdisk-photorec git vscode putty filezilla wireshark postman boxstarter ffmpeg tor-browser qbittorrent openvpn rufus obs-studio bitwarden foobar2000 -y
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Commandant
-It's not Commando but it might be close
+## Commandant
+
+It's not Commando-vm but it might be close..... one day
+
+---
+
+Consider this initial upload alpha quality. It works eventually but needs some more work to be complete
+
+This tool is very much insipred by Commando, attempting to fill the void in my heart while Commando undergoes some major changes (will they ever release again? I'm not sure, their install scripts dont work out for me).
+
+I wrote this tool for a job and the employer was nice enough to let me publically host this script. It is by no means complete or good, but I hope you find some use in it. Even if it's a quick way to provision windows 11 as an attack box. Feel free to fork or contribute.
+
+#### Why make this?
+
+Because Flare and Commando have failed to install so many times that i'm not sure whether i'm stupid or they are actually broken
+
+---
+
+### SysReq
+
+- Windows 11 22H2 Pro or something similar
+
+- Administrator access
+
+- Powershell - The one included in Windows will do fine
+
+- Preferably 8GB+ of RAM (tested with 8GB, 4GB should do accordin to taskmanager)
+
+- HDD/SSD space will be determined by how much you install, I havent tested but i'd expect 10-15GB to be enough (probably way too much)
+
+- A MS Account that signs into the MS Store (havin the account sign into apps but not the system works just fine)
+
+#### Notes:
+
+This is my first powershell project, please open an issue if something doeesnt work and i'll accomodate as best I can.
+
+This tool has not been thoroughly tested but should work anywhere you can get WSL2 and WSLg on Windows. **In theory** Windows 10 21H1 or newer should work.
