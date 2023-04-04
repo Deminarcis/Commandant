08b72ac76be1c1d30cde1f69140e2f4410121b8c
Deminarcis <jake.maher.10@gmail.com>
2023-04-05 01:51:18 +1000

    Altered tools that choco installs. Added powershell profile

--- Commandant.ps1
+++ Commandant.ps1
@@ -44,6 +44,54 @@ winget install microsoft.powershell --accept-package-agreements -h --accept-sour
 Install-Package chocolatey
 Start-Process powershell -ArgumentList {
     choco upgrade chocolatey -y
-    choco install firefox sysinternals ChocolateyGUI adobereader vlc python 7zip testdisk-photorec git vscode putty filezilla wireshark postman boxstarter ffmpeg tor-browser transmission openvpn rufus obs-studio bitwarden foobar2000 obsidian -y
-    choco install virtualbox virtualbox-guest-additions-guest.install
+    choco install firefox sysinternals ChocolateyGUI FoxitReader vlc python 7zip testdisk-photorec git vscode filezilla wireshark postman boxstarter ffmpeg tor-browser qbittorrent openvpn rufus obs-studio bitwarden obsidian virtualbox veracrypt synctrayzor -y 
 } -Verb RunAs
+
+powershelProfile = powershell /c Write-Host $PROFILE
+
+Write-Host @"
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+}
+
+Import-Module PSWindowsUpdate
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+function prompt {
+    $Time = (Get-Date).ToString("hh:mm") 
+    $host.ui.rawui.WindowTitle = (Get-Location)
+    # The following block is surrounded by two delimiters.
+    # These delimiters must not be modified. Thanks.
+    # START KALI CONFIG VARIABLES
+    $PROMPT_ALTERNATIVE='twoline'
+    $NEWLINE_BEFORE_PROMPT='no'
+    # STOP KALI CONFIG VARIABLES
+    
+      $esc = [char]27
+      $bell = [char]7
+      $bold = "$esc[1m"
+      $reset = "$esc[0m"
+      If ($NEWLINE_BEFORE_PROMPT -eq 'yes') { Write-Host }
+      If ($PROMPT_ALTERNATIVE -eq 'twoline') {
+        Write-Host "┌──[" -NoNewLine -ForegroundColor Magenta
+        Write-Host "${bold}$([environment]::username)" -NoNewLine -ForegroundColor Red
+        Write-Host "] 💀 [" -NoNewLine -ForegroundColor Magenta
+        Write-Host "@$([system.environment]::MachineName)${reset}" -NoNewLine -ForegroundColor Green
+        Write-Host " - $Time " -NoNewLine -ForegroundColor Blue
+        Write-Host "]-[" -NoNewLine -ForegroundColor Magenta
+        Write-Host "${bold}$(Get-Location)${reset}" -NoNewLine -ForegroundColor White
+        Write-Host "]" -ForegroundColor Magenta
+        Write-Host "└─" -NoNewLine -ForegroundColor Magenta
+        Write-Host "${reset}" -NoNewLine -ForegroundColor Magenta
+      } Else {
+        Write-Host "${bold}PS " -NoNewLine -ForegroundColor Magenta
+        Write-Host "$([environment]::username)@$([system.environment]::MachineName) " -NoNewLine -ForegroundColor Magenta
+        Write-Host "$(Get-Location)>${reset}" -NoNewLine -ForegroundColor Magenta
+      }
+      # Terminal title
+      Write-Host "${esc}]0;PS> $([environment]::username)@$([system.environment]::MachineName): $(Get-Location)${bell}" -NoNewLine
+      return " "
+}
+"@ > $powershelProfile
