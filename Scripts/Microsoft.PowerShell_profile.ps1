4bc1a49b81d6760351b674cac4ddd5cc7ad4fe0c
Deminarcis.rb <jake.maher.10@gmail.com>
2024-03-28 15:45:41 +1100

    Better Readline handling on Linux Added modules to make windows CLI less crap

--- Scripts/Microsoft.PowerShell_profile.ps1
+++ Scripts/Microsoft.PowerShell_profile.ps1
@@ -10,10 +10,20 @@ if ($IsWindows) {
   Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery
   Install-Module -Name TabExpansionPlusPlus -Scope CurrentUser -Repository PSGallery -AllowClobber
   Install-Module -Name PSUtil -Scope CurrentUser -Repository PSGallery -AllowClobber
+  Install-Module -Name ImportExcel -Scope CurrentUser -Repository PSGallery -AllowClobber
+  Install-Module -Name PSWriteHTML -Scope CurrentUser -Repository PSGallery -AllowClobber
+  Install-Module -Name PSWritePDF -Scope CurrentUser -Repository PSGallery -AllowClobber -Force
+  Install-Module -Name PSWriteWord -Scope CurrentUser -Repository PSGallery -AllowClobber
+  Install-Module -Name PSScriptTools -Scope CurrentUser -Repository PSGallery -AllowClobber
   Import-Module Terminal-Icons
   Import-Module PSUtil
   Import-Module Pscx
   Import-Module TabExpansionPlusPlus
+  Import-Module ImportExcel
+  Import-Module PSWriteHTML
+  Import-Module PSWritePDF
+  Import-Module PSWriteWord
+  Import-Module PSScriptTools
 }
 else {
   Install-Module -Name PSDepend -Scope CurrentUser -Repository PSGallery
@@ -21,7 +31,13 @@ else {
   Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery
   Install-Module -Name TabExpansionPlusPlus -Scope CurrentUser -Repository PSGallery -AllowClobber
   Install-Module -Name PSUtil -Scope CurrentUser -Repository PSGallery -AllowClobber
+  Install-Module -Name PSUnixUtilCompleters -Scope CurrentUser -Repository PSGallery -AllowClobber
+  Install-Module -Name PSScriptTools -Scope CurrentUser -Repository PSGallery -AllowClobber
+  Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+  $env:COMPLETION_SHELL_PREFERENCE = '/usr/bin/zsh'
+  Import-Module PSUnixUtilCompleters
   Import-Module Terminal-Icons
+  Import-Module PSScriptTools
   Import-Module PSUtil
   Import-Module TabExpansionPlusPlus
 }
