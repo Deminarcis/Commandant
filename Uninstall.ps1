bb790a33f525c18936ba6620c27c964e43346164
Deminarcis.rb <jake.maher.10@gmail.com>
2024-04-01 14:30:05 +1100

    Added comments to identify product codes

--- Uninstall.ps1
+++ Uninstall.ps1
@@ -12,9 +12,13 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 ################
 Write-Output '[!!] Uninstalling everything Commandant has done to your PC'
 wsl.exe --uninstall
+### Kali
 winget uninstall  9PKR34TNCV07 -s msstore
+### Ubuntu
 winget uninstall  9PDXGNCFSCZV -s msstore
-winget uninstall  9P7KNL5RWT26 -s msstore
+### Sysinternals
+winget uninstall  9P7KNL5RWT25 -s msstore
+### Python
 winget uninstall  9NCVDN91XZQP -s msstore
 winget uninstall 'Microsoft.Powershell' 'Git.git' '7zip.7zip' 'Microsoft.VisualStudioCode' 'Microsoft.Powertoys' 'Microsoft.DevHome' 'Mozilla.Firefox' 'Mozilla.Thunderbird' 'Oracle.Virtualbox' 'Jetbrains.Toolbox' 'GNU.Nano' 'sharkdp.bat'
 Remove-Item '$env:USERPROFILE\bzImage'
