7b3c4a5e4975e4e17fca6d9e607251be2d3fb43c
Deminarcis <jake.maher.10@gmail.com>
2024-07-21 16:56:02 +1000

    Update Microsoft.PowerShell_profile.ps1
    faster loading powershell profile. also modules one installed when asked -- alias set to allow that

--- Scripts/Microsoft.PowerShell_profile.ps1
+++ Scripts/Microsoft.PowerShell_profile.ps1
@@ -2,42 +2,33 @@
 {
     Import-Module PSReadLine
 }
-
-if ($IsWindows) {
-  Install-Module -Name PSDepend -Scope CurrentUser -Repository PSGallery
-  Import-Module PSDepend
-  Install-Module Pscx -Scope CurrentUser -AllowClobber
-  Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery
-  Install-Module -Name TabExpansionPlusPlus -Scope CurrentUser -Repository PSGallery -AllowClobber
-  Install-Module -Name PSUtil -Scope CurrentUser -Repository PSGallery -AllowClobber
-  Install-Module -Name ImportExcel -Scope CurrentUser -Repository PSGallery -AllowClobber
-  Install-Module -Name PSWriteHTML -Scope CurrentUser -Repository PSGallery -AllowClobber
-  Install-Module -Name PSWritePDF -Scope CurrentUser -Repository PSGallery -AllowClobber -Force
-  Install-Module -Name PSWriteWord -Scope CurrentUser -Repository PSGallery -AllowClobber
-  Import-Module Terminal-Icons
-  Import-Module PSUtil
-  Import-Module Pscx
-  Import-Module TabExpansionPlusPlus
-  Import-Module ImportExcel
-  Import-Module PSWriteHTML
-  Import-Module PSWritePDF
-  Import-Module PSWriteWord
-  Import-Module PSScriptTools
-}
-else {
-  Install-Module -Name PSDepend -Scope CurrentUser -Repository PSGallery
-  Import-Module PSDepend
-  Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery
-  Install-Module -Name TabExpansionPlusPlus -Scope CurrentUser -Repository PSGallery -AllowClobber
-  Install-Module -Name PSUtil -Scope CurrentUser -Repository PSGallery -AllowClobber
-  Install-Module -Name PSUnixUtilCompleters -Scope CurrentUser -Repository PSGallery -AllowClobber
-  Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-  $env:COMPLETION_SHELL_PREFERENCE = '/usr/bin/zsh'
-  Import-Module PSUnixUtilCompleters
-  Import-Module Terminal-Icons
-  Import-Module PSScriptTools
-  Import-Module PSUtil
-  Import-Module TabExpansionPlusPlus
+function modules {
+  if ($IsWindows) {
+    Install-Module -Name PSDepend -Scope CurrentUser -Repository PSGallery
+    Import-Module PSDepend
+    Install-Module Pscx -Scope CurrentUser -AllowClobber
+    Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery
+    Install-Module -Name TabExpansionPlusPlus -Scope CurrentUser -Repository PSGallery -AllowClobber
+    Install-Module -Name PSUtil -Scope CurrentUser -Repository PSGallery -AllowClobber
+    Import-Module Terminal-Icons
+    Import-Module PSUtil
+    Import-Module Pscx
+    Import-Module TabExpansionPlusPlus
+  }
+  else {
+    Install-Module -Name PSDepend -Scope CurrentUser -Repository PSGallery
+    Import-Module PSDepend
+    Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery
+    Install-Module -Name TabExpansionPlusPlus -Scope CurrentUser -Repository PSGallery -AllowClobber
+    Install-Module -Name PSUtil -Scope CurrentUser -Repository PSGallery -AllowClobber
+    Install-Module -Name PSUnixUtilCompleters -Scope CurrentUser -Repository PSGallery -AllowClobber
+    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+    $env:COMPLETION_SHELL_PREFERENCE = '/usr/bin/zsh'
+    Import-Module PSUnixUtilCompleters
+    Import-Module Terminal-Icons
+    Import-Module PSUtil
+    Import-Module TabExpansionPlusPlus
+  }
 }
 
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
@@ -76,3 +67,5 @@ function prompt {
       Write-Host "${esc}]0;PS> $([environment]::username)@$([system.environment]::MachineName): $(Get-Location)${bell}" -NoNewLine
       return " "
 }
+### Aliases
+Set-Alias update-modules modules
