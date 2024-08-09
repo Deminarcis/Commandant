1c0af618c51269f03ff2ec3b1e52ce3fde253aa8
Deminarcis <jake.maher.10@gmail.com>
2024-08-09 21:24:26 +1000

    Update Microsoft.PowerShell_profile.ps1

--- Scripts/Microsoft.PowerShell_profile.ps1
+++ Scripts/Microsoft.PowerShell_profile.ps1
@@ -21,7 +21,6 @@ function modules {
     Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery
     Install-Module -Name TabExpansionPlusPlus -Scope CurrentUser -Repository PSGallery -AllowClobber
     Install-Module -Name PSUtil -Scope CurrentUser -Repository PSGallery -AllowClobber
-    Install-Module -Name PSUnixUtilCompleters -Scope CurrentUser -Repository PSGallery -AllowClobber
     Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
     $env:COMPLETION_SHELL_PREFERENCE = '/usr/bin/zsh'
     Import-Module PSUnixUtilCompleters
@@ -33,7 +32,42 @@ function modules {
 
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+#Prompt V1
+#function prompt {
+#    $Time = (Get-Date).ToString("HH:mm")
+#    $host.ui.rawui.WindowTitle = (Get-Location)
+#    # START CONFIG VARIABLES
+#    $PROMPT_ALTERNATIVE='twoline'
+#    $NEWLINE_BEFORE_PROMPT='no'
+#    # STOP CONFIG VARIABLES
+#
+#     $esc = [char]27
+#      $bell = [char]7
+#      $bold = "$esc[1m"
+#      $reset = "$esc[0m"
+#      If ($NEWLINE_BEFORE_PROMPT -eq 'yes') { Write-Host }
+#      If ($PROMPT_ALTERNATIVE -eq 'twoline') {
+#        Write-Host "┌──[" -NoNewLine -ForegroundColor Magenta
+#        Write-Host "${bold}$([environment]::username)" -NoNewLine -ForegroundColor Red
+#        Write-Host "]-[ 💀 ]-[" -NoNewLine -ForegroundColor Magenta
+#        Write-Host "@$([system.environment]::MachineName)${reset}" -NoNewLine -ForegroundColor Yellow
+#        Write-Host " - $Time " -NoNewLine -ForegroundColor Cyan
+#        Write-Host "]-[" -NoNewLine -ForegroundColor Magenta
+#        Write-Host "${bold}$(Get-Location)${reset}" -NoNewLine -ForegroundColor White
+#        Write-Host "]" -ForegroundColor Magenta
+#        Write-Host "└─" -NoNewLine -ForegroundColor Magenta
+#        Write-Host "${reset}" -NoNewLine -ForegroundColor Magenta
+#      } Else {
+#        Write-Host "${bold}PS " -NoNewLine -ForegroundColor Magenta
+#        Write-Host "$([environment]::username)@$([system.environment]::MachineName) " -NoNewLine -ForegroundColor Magenta
+#        Write-Host "$(Get-Location)>${reset}" -NoNewLine -ForegroundColor Magenta
+#      }
+#      # Terminal title
+#      Write-Host "${esc}]0;PS> $([environment]::username)@$([system.environment]::MachineName): $(Get-Location)${bell}" -NoNewLine
+#      return " "
+#}
 
+#Prompt V2 (no icons)
 function prompt {
     $Time = (Get-Date).ToString("HH:mm")
     $host.ui.rawui.WindowTitle = (Get-Location)
@@ -42,7 +76,7 @@ function prompt {
     $NEWLINE_BEFORE_PROMPT='no'
     # STOP CONFIG VARIABLES
 
-      $esc = [char]27
+     $esc = [char]27
       $bell = [char]7
       $bold = "$esc[1m"
       $reset = "$esc[0m"
@@ -50,7 +84,7 @@ function prompt {
       If ($PROMPT_ALTERNATIVE -eq 'twoline') {
         Write-Host "┌──[" -NoNewLine -ForegroundColor Magenta
         Write-Host "${bold}$([environment]::username)" -NoNewLine -ForegroundColor Red
-        Write-Host "] 💀 [" -NoNewLine -ForegroundColor Magenta
+        Write-Host "]--[" -NoNewLine -ForegroundColor Magenta
         Write-Host "@$([system.environment]::MachineName)${reset}" -NoNewLine -ForegroundColor Yellow
         Write-Host " - $Time " -NoNewLine -ForegroundColor Cyan
         Write-Host "]-[" -NoNewLine -ForegroundColor Magenta
