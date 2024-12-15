cf34ea605b0c587c57fbf51672962a42df92e631
Deminarcis <jake.maher.10@gmail.com>
2024-12-15 21:31:54 +1100

    ...

--- bin/install_extras.ps1
+++ bin/install_extras.ps1
@@ -2,5 +2,5 @@
 Write-Output '[+] Installing kali-linux from MS store'
 winget install  9PKR34TNCV07 -s msstore --accept-package-agreements -h --accept-source-agreements
 Write-Output '[!!] Please close the following two terminals manually when each step is done (scripts will tell you if they can) [!!]'
-kali.exe | Out-Null
-start-process powershell -ArgumentList "-wait -noexit -command wsl.exe -d kali-linux --exec '../Scripts/configure_wsl.sh'" | Out-Null
+wsl.exe -d kali-linux --exec 'apt update && apt full-upgrade' | Out-Null
+wsl.exe -d kali-linux --exec ../Scripts/configure_wsl.sh | Out-Null
